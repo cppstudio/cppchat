7e2f46574ee028f33653db59fd8a15ef7a441fa7
DealiAxy <dealiaxy@gmail.com>
2019-04-23 16:36:37 +0800

    基本实现了朋友圈

--- lib/page/friend_circle.dart
+++ lib/page/friend_circle.dart
@@ -33,25 +33,41 @@ class _FriendCirclePageState extends State<FriendCirclePage> {
       print('朋友圈下拉刷新');
     });
     _circleList.clear();
-    _circleList.add(
-      FriendCircleItem(
-        username: '用户',
-        content: '内容',
-        images: [
-          Image.network(
-            'http://lorempixel.com/80/80/',
-            fit: BoxFit.cover,
-          ),
-        ],
-        timeStr: '1分钟前',
-        from: 'IT之家客户端',
-        avatar: Image.network(
-          'http://lorempixel.com/35/35/',
-          fit: BoxFit.fitHeight,
-          height: 35,
+    _circleList.add(FriendCircleItem(
+      username: '用户',
+      content: '内容',
+      images: [
+        Image.network(
+          'http://lorempixel.com/80/80/',
+          fit: BoxFit.cover,
         ),
-      )
-    );
+      ],
+      timeStr: '1分钟前',
+      from: 'IT之家客户端',
+      avatar: Image.network(
+        'http://lorempixel.com/35/35/',
+        fit: BoxFit.fitHeight,
+        height: 35,
+      ),
+    ));
+
+    _circleList.add(FriendCircleItem(
+      username: '用户',
+      content: '内容',
+      images: [
+        Image.network('http://lorempixel.com/80/80/', fit: BoxFit.cover),
+        Image.network('http://lorempixel.com/80/80/', fit: BoxFit.cover),
+        Image.network('http://lorempixel.com/80/80/', fit: BoxFit.cover),
+        Image.network('http://lorempixel.com/80/80/', fit: BoxFit.cover),
+      ],
+      timeStr: '1分钟前',
+      from: 'IT之家客户端',
+      avatar: Image.network(
+        'http://lorempixel.com/35/35/',
+        fit: BoxFit.fitHeight,
+        height: 35,
+      ),
+    ));
 
     for (var i = 0; i < 10; i++) {
       _circleList.add(FriendCircleItem(
@@ -78,7 +94,7 @@ class _FriendCirclePageState extends State<FriendCirclePage> {
         ),
       ));
     }
-  
+
     setState(() {});
   }
 
@@ -158,6 +174,8 @@ class _FriendCirclePageState extends State<FriendCirclePage> {
                         )),
                   ),
                   SimpleDivider(height: 0, width: 10),
+
+                  /// 大头像要加上一点点圆边
                   ClipRRect(
                     borderRadius: BorderRadius.circular(6.0),
                     child: Image.network('http://lorempixel.com/80/80/'),
@@ -173,28 +191,191 @@ class _FriendCirclePageState extends State<FriendCirclePage> {
 
   Widget _buildFriendCircleItem(FriendCircleItem item) {
     var itemChildren = <Widget>[
+      /// 用户名
       Text(
         item.username,
-        style: TextStyle(fontSize: 16, color: Colors.blue[900]),
+        style: TextStyle(fontSize: 15, color: Colors.blue[900]),
       ),
-      Text(item.content),
+
+      /// 朋友圈内容
+      Container(
+        padding: EdgeInsets.only(bottom: 3),
+        child: Text(
+          item.content,
+          style: TextStyle(fontSize: 14),
+        ),
+      ),
     ];
 
-    if (item.images.length == 1)
-      itemChildren.add(item.images[0]);
-    else if (item.images.length == 2)
-      itemChildren.add(Row(
-        children: <Widget>[item.images[0], item.images[1]],
-      ));
+    /// 暴力的强行图片布局
+    switch (item.images.length) {
+      case 1:
+        itemChildren.add(item.images[0]);
+        break;
+      case 2:
+        itemChildren.add(Row(children: <Widget>[
+          item.images[0],
+          SimpleDivider(height: 0, width: 3),
+          item.images[1],
+        ]));
+        break;
+      case 3:
+        itemChildren
+          ..add(Row(children: <Widget>[
+            item.images[0],
+            SimpleDivider(height: 0, width: 3),
+            item.images[1],
+          ]))
+          ..add(SimpleDivider(height: 3, width: 0))
+          ..add(Row(children: <Widget>[item.images[2]]));
+        break;
+      case 4:
+        itemChildren
+          ..add(Row(children: <Widget>[
+            item.images[0],
+            SimpleDivider(height: 0, width: 3),
+            item.images[1]
+          ]))
+          ..add(SimpleDivider(height: 3, width: 0))
+          ..add(Row(children: <Widget>[
+            item.images[2],
+            SimpleDivider(height: 0, width: 3),
+            item.images[3]
+          ]));
+        break;
+      case 5:
+        itemChildren
+          ..add(Row(children: <Widget>[
+            item.images[0],
+            SimpleDivider(height: 0, width: 3),
+            item.images[1],
+            SimpleDivider(height: 0, width: 3),
+            item.images[2]
+          ]))
+          ..add(SimpleDivider(height: 3, width: 0))
+          ..add(Row(children: <Widget>[
+            item.images[3],
+            SimpleDivider(height: 0, width: 3),
+            item.images[4]
+          ]));
+        break;
+      case 6:
+        itemChildren
+          ..add(Row(children: <Widget>[
+            item.images[0],
+            SimpleDivider(height: 0, width: 3),
+            item.images[1],
+            SimpleDivider(height: 0, width: 3),
+            item.images[2]
+          ]))
+          ..add(SimpleDivider(height: 3, width: 0))
+          ..add(Row(children: <Widget>[
+            item.images[3],
+            SimpleDivider(height: 0, width: 3),
+            item.images[4],
+            SimpleDivider(height: 0, width: 3),
+            item.images[5]
+          ]));
+        break;
+      case 7:
+        itemChildren
+          ..add(Row(children: <Widget>[
+            item.images[0],
+            SimpleDivider(height: 0, width: 3),
+            item.images[1],
+            SimpleDivider(height: 0, width: 3),
+            item.images[2]
+          ]))
+          ..add(SimpleDivider(height: 3, width: 0))
+          ..add(Row(children: <Widget>[
+            item.images[3],
+            SimpleDivider(height: 0, width: 3),
+            item.images[4],
+            SimpleDivider(height: 0, width: 3),
+            item.images[5]
+          ]))
+          ..add(SimpleDivider(height: 3, width: 0))
+          ..add(Row(
+            children: <Widget>[item.images[6]],
+          ));
+        break;
+      case 8:
+        itemChildren
+          ..add(Row(children: <Widget>[
+            item.images[0],
+            SimpleDivider(height: 0, width: 3),
+            item.images[1],
+            SimpleDivider(height: 0, width: 3),
+            item.images[2]
+          ]))
+          ..add(SimpleDivider(height: 3, width: 0))
+          ..add(Row(children: <Widget>[
+            item.images[3],
+            SimpleDivider(height: 0, width: 3),
+            item.images[4],
+            SimpleDivider(height: 0, width: 3),
+            item.images[5]
+          ]))
+          ..add(SimpleDivider(height: 3, width: 0))
+          ..add(Row(
+            children: <Widget>[item.images[6], item.images[7]],
+          ));
+        break;
+      case 9:
+        itemChildren
+          ..add(Row(children: <Widget>[
+            item.images[0],
+            SimpleDivider(height: 0, width: 3),
+            item.images[1],
+            SimpleDivider(height: 0, width: 3),
+            item.images[2]
+          ]))
+          ..add(SimpleDivider(height: 3, width: 0))
+          ..add(Row(children: <Widget>[
+            item.images[3],
+            SimpleDivider(height: 0, width: 3),
+            item.images[4],
+            SimpleDivider(height: 0, width: 3),
+            item.images[5]
+          ]))
+          ..add(SimpleDivider(height: 3, width: 0))
+          ..add(Row(
+            children: <Widget>[
+              item.images[6],
+              SimpleDivider(height: 0, width: 3),
+              item.images[7],
+              SimpleDivider(height: 0, width: 3),
+              item.images[8]
+            ],
+          ));
+        break;
+    }
+
+    /// 发布时间、来源、操作
+    itemChildren.add(Row(
+      children: <Widget>[
+        Text(item.timeStr, style: TextStyle(fontSize: 12)),
+        SimpleDivider(height: 0, width: 12),
+        // Expanded(child: Text(item.from, style: TextStyle(fontSize: 12))),
+        Text(item.from, style: TextStyle(fontSize: 12)),
+        FlatButton(child: Icon(Icons.more_horiz), onPressed: () {}),
+      ],
+    ));
 
     var row = Row(
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
+        /// 头像
         Container(
-          padding: EdgeInsets.fromLTRB(8, 0, 8, 0),
-          child: item.avatar,
+          padding: EdgeInsets.fromLTRB(8, 3, 8, 0),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(6),
+            child: item.avatar,
+          ),
         ),
+
+        /// 朋友圈主体
         Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -203,6 +384,11 @@ class _FriendCirclePageState extends State<FriendCirclePage> {
       ],
     );
 
-    return row;
+    var column = Column(children: <Widget>[
+      row,
+      Divider(height: 10),
+    ]);
+
+    return column;
   }
 }
